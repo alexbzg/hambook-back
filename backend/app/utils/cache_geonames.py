cd7b46a05ff637b853a2d6574f6c4e00fcce9f6e
root <alexbzg@gmail.com>
2023-03-12 15:16:54 +0300

    geonames_cache: store cities

--- backend/app/utils/cache_geonames.py
+++ backend/app/utils/cache_geonames.py
@@ -5,6 +5,8 @@ import json
 from pathlib import Path
 from collections import defaultdict
 import re
+from urllib.request import urlretrieve
+import zipfile
 
 import requests
 
@@ -20,6 +22,7 @@ for country_line in countries_src.split("\n"):
         try:
             value, _, _, _, label, _ = country_line.split("\t", 5)
             countries.append({'value': value, 'label': label})
+            Path(f"{DST_DIR}{value}").mkdir(exist_ok=True)
         except:
             print(country_line)
 countries.sort(key=lambda x: x['label'])
@@ -44,20 +47,41 @@ for country in regions:
         json.dump(regions[country], regions_dst)
 
 districts_src = requests.get(f"{SRC_DIR}admin2Codes.txt").text
-districts = defaultdict(lambda: defaultdict(list))
+geodata = defaultdict(lambda: defaultdict(lambda: {'districts': [], 'cities': []}))
 RE_DISTRICT = re.compile(r"^(\w+)\.(\w+)\.(\w+)\t([^\t]+)\t.*$")
 
 for district_line in districts_src.split("\n"):
     match = RE_DISTRICT.match(district_line)
     if match:
-        districts[match.group(1)][match.group(2)].append({'value': match.group(3), 'label': match.group(4)})
+        geodata[match.group(1)][match.group(2)]['districts'].append(
+                {'value': match.group(3), 'label': match.group(4)})
     else:
         print(district_line)
-for country in districts:
+
+objects_zip_path = f"{DST_DIR}allCountries.zip"
+urlretrieve(f"{SRC_DIR}allCountries.zip", objects_zip_path)
+
+with zipfile.ZipFile(objects_zip_path, 'r') as zip_file:
+    zip_file.extractall(DST_DIR)
+Path(objects_zip_path).unlink()
+
+objects_path = f"{DST_DIR}allCountries.txt"
+
+with open(objects_path, 'r', encoding='utf-8') as objects_file:
+    for object_line in objects_file:
+        object = object_line.split("\t")
+        if object[6] == 'P':
+            geodata[object[8]][object[10] or '00']['cities'].append(object[1])
+
+for country in geodata:
     country_dir = f"{DST_DIR}{country}"
-    for region in districts[country]:
-        districts[country][region].sort(key=lambda x: x['label'])
-        with open(f"{country_dir}/{region}.json", "w", encoding="utf-8") as districts_dst:
-            json.dump(districts[country][region], districts_dst)
+    for region in geodata[country]:
+        geodata[country][region]['districts'].sort(key=lambda x: x['label'])
+        geodata[country][region]['cities'] = list(set(geodata[country][region]['cities']))
+        geodata[country][region]['cities'].sort()
+        with open(f"{country_dir}/{region}.json", "w", encoding="utf-8") as geodata_dst:
+            json.dump(geodata[country][region], geodata_dst)
+
+
 
 
